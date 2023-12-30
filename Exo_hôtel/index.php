e8bfeec1977151b636d8d0e1be682d10f9f2ce25
Asma S <saidiasma422@gmail.com>
2023-12-30 21:49:32 +0100

    client1 commit

--- Exo_hôtel/index.php
+++ Exo_hôtel/index.php
@@ -14,7 +14,7 @@
     require("client.php");
     require("chambre.php");
 
-    $client1= new Client("Saidi ","Asma ","12 rue de la joie ","07.65.45.34.51");
+    $client1= new Client("Saidi ","Asma ","12 rue de la folie ","07.65.45.34.51");
 
     $Hilton = new Hotel("Hilton ","10 route de la gare, Strabourg ",30, 3, 27);
     echo $Hilton->afficherInfos();
